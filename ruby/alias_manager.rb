b042066e1624b0e4beb1f83a7061f1cfce013282
Mike Bonetti <mbonettimedia@gmail.com>
2017-01-15 21:24:20 -0600

    Add letter changing logic and name vault

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,25 +4,52 @@
 #string or array containing 'aeiou'
 #Do the same except for consonants
 #print the result!
-
-puts "Hello, please enter your first and last names. 
-(Make sure nobody is looking over your shoulder)"
-real_name = gets.chomp
-real_array = real_name.downcase.split(' ')
-p real_name
-p real_array
-swap_array = real_array[1], real_array[0]
-p swap_array
-name_letters = swap_array.join(' ').chars
-p name_letters
-name_letters.each do |letter|
-  vowels = ["a","e","i","o","u"]
-  consonants = ["b","c","d","f","g","h","j","k","l","m","n",
-    "p","q","r","s","t","v","x","z","w","y"]
-  if vowels.include?(letter)
-    puts "#{letter} is a vowel"
-  elsif consonants.include?(letter)
-    puts "#{letter} is a consonant"
+super_top_secret_identity_vault = []
+counter = 0
+while counter == 0
+  spy_name = []
+  puts "Hello, please enter your first and last names. 
+  (Or enter 'exit' to quit the program)"
+  real_name = gets.chomp
+  if real_name == "exit"
+    counter = 1
+    break
+  else 
+    real_array = real_name.downcase.split(' ')
+    swap_array = real_array[1], real_array[0]
+    name_letters = swap_array.join(' ').chars
+    name_letters.each do |letter|
+      vowels = ["a","e","i","o","u"]
+      consonants = ["b","c","d","f","g","h","j","k","l","m","n",
+        "p","q","r","s","t","v","x","z","w","y"]
+      if vowels.include?(letter)
+        letter_index = vowels.index(letter)
+        if letter_index + 1 < vowels.length
+          new_letter = vowels[letter_index + 1]
+          spy_name.push(new_letter)
+        else
+          new_letter = vowels[0]
+          spy_name.push(new_letter)
+        end
+      elsif consonants.include?(letter)
+        letter_index = consonants.index(letter)
+        if letter_index + 1 < consonants.length
+          new_letter = consonants[letter_index + 1]
+          spy_name.push(new_letter)
+        else
+          new_letter = consonants[0]
+          spy_name.push(new_letter)
+        end
+      else
+        new_letter = " "
+        spy_name.push(new_letter)
+      end
   end
 end
+  spy_name = spy_name.join('')
+  official_spy_name = spy_name.split.map(&:capitalize).join(' ')
+  puts "Your new identity is #{official_spy_name}. Hope you like it because now you're stuck with it, agent!"
+  super_top_secret_identity_vault.push(official_spy_name + " is actually: "+ real_name)
+end
+puts super_top_secret_identity_vault
 
