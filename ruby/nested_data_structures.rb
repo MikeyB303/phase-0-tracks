0fadd15316d667d5d046433798799a96b810e30d
Mike Bonetti <mbonettimedia@gmail.com>
2017-01-16 08:34:08 -0600

    Add nested data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,26 @@
+pantry = {
+    top_shelf: {
+        food: [
+            "Ceareal", 
+            "chips",
+            "Cookies"
+        ]
+    },
+    middle_shelf: {
+        baking_supplies: [
+            "FLour", 
+            "Sugar",
+            "Baking Soda"
+        ]
+    },
+    coach: {
+        plastic_bag_pile: [
+          "bag1",
+          "bag2",
+          "bag3"
+          ]
+    }
+}
+
+pantry[:top_shelf][:food]
+pantry[:top_shelf][:food][2]
