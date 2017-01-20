1efd8635eed6dd8b632d903981a3a7997b9d82e9
Mike Bonetti <mbonettimedia@gmail.com>
2017-01-20 16:28:17 -0600

    Add initialize characteristics

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,13 +8,17 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance"
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end  
 
 end
 
-santa = Santa.new
+santa = Santa.new("agender", "Japanese")
 
 santa.speak
-santa.eat_milk_and_cookies("cookie")
+santa.eat_milk_and_cookies("double chocolate chip crunch")
