fcb415df2d7c054464bb94558fe929094439d01f
Mike Bonetti <mbonettimedia@gmail.com>
2017-01-23 02:03:02 -0600

    Add game class and functionality

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -2,14 +2,20 @@
 
 class Word_Game
   attr_reader :blank_array
+  attr_reader :letters_array
+  attr_reader :is_over
+  attr_reader :guesses
+  attr_reader :guess_count
 
   def initialize(input)
-    @guess_count = 0
+    @guess_count = input.length
+    @guesses = 0
     @is_over = false
     @word = input
     @letters_array = []
     @blank_array = []
     @letter_index = nil
+    @guess_bank = []
   end
 
   def letters
@@ -18,18 +24,30 @@ class Word_Game
   end
 
   def blank
-    @blank_array = @letters_array.each do |letter|
+    @letters_array.each do |letter|
       if letter != " " 
-        @letter_index = @letters_array.index(letter)
-        @letters_array[@letter_index] = "_"
+        @blank_array.push("_")
+      else
+        @blank_array.push(" ")
       end
     end
   end
 
-  def guess(letter)
-    if
+  def guess(guess)
+    if @guess_bank.include?(guess)
+      puts "You already guessed that letter!"
+    elsif @letters_array.include?(guess)
+      correct_index = @letters_array.each.with_index.find_all{ |a,i| a == "#{guess}"}.map{ |a,b| b }
+      correct_index.each do |index|
+        @blank_array[index] = guess
+      end
 
-  end 
+    else
+      puts "That letter does not belong to the word or phrase. Try again!"
+      @guesses += 1
+      @guess_bank.push(guess)
+    end
+  end
 end
 
 
@@ -40,3 +58,14 @@ game = Word_Game.new(gets.chomp)
 game.letters
 game.blank
 p game.blank_array
+until game.letters_array == game.blank_array|| game.guesses >= game.guess_count
+  puts "guess a letter. #{game.guess_count - game.guesses} guess(es) remaining"
+  game.guess(gets.chomp)
+  p game.blank_array
+end
+if game.letters_array == game.blank_array
+  puts "Great job!! I knew you could do it!"
+end
+if game.guesses >= game.guess_count
+  puts "Hahaha! I knew you wouldn't get it!"
+end
